ba3027f423f40fa2b4261ada11eff00e6f6e927f
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-07-20 11:37:05 +0200

    aggiunta variante switch

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ btnGenerazione.addEventListener("click", function(){
     document.getElementById('codcp').innerHTML = Math.floor(Math.random() * (99999 - 90000)) + 90000;
     document.getElementById('costo').innerHTML = prezzoFinale.toPrecision(3) + '€';
 
-    ticket.className = 'ticket show';
+    ticket.className = 'ticket show clearfix';
 
 }
 );
@@ -61,7 +61,7 @@ btnAnnullamento.addEventListener("click", function(){
     var ticket = document.getElementById('ticket');
     document.getElementById('anagrafica').value = '';
     document.getElementById('kilometri').value = '';
-    ticket.className = 'ticket hidden';
+    ticket.className = 'ticket hidden clearfix';
 }
 );
 
@@ -71,14 +71,32 @@ btnTemplate.addEventListener("click", function(){
 
     var scren = document.getElementById('scren');
 
-    if (template == 'Default') {
-        scren.className = 'screen default';
-    }  else if (template == 'Flower') {
-        scren.className = 'screen flower';
-    }  else if (template == 'Lion') {
-        scren.className = 'screen lion';
-    }  else if(template == 'Water'){
-        scren.className = 'screen water';
+    // variante if(non consigliata)
+
+    // if (template == 'Default') {
+    //     scren.className = 'screen default';
+    // }  else if (template == 'Flower') {
+    //     scren.className = 'screen flower';
+    // }  else if (template == 'Lion') {
+    //     scren.className = 'screen lion';
+    // }  else if(template == 'Water'){
+    //     scren.className = 'screen water';
+    // }
+
+    // variante switch(ideale)
+
+    switch (template) {
+        case 'Flower':
+            scren.className = 'screen flower';
+            break;
+        case 'Lion':
+            scren.className = 'screen lion';
+            break;
+        case 'Water':
+            scren.className = 'screen water';
+            break;
+        default:
+            scren.className = 'screen default';
     }
 }
 );
